3d77e2c2f2963573275596d22c308482e4952063
Dinis Afonso Cerqueira Galvão <up202207217@edu.fe.up.pt>
2025-02-20 09:06:00 +0000

    1.1 done

---
+++ tp1/MyTriangle.js
@@ -0,0 +1,30 @@
+import { CGFobject } from "../lib/CGF.js";
+
+/**
+ * MyTriangle
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+
+export class MyTriangle extends CGFobject {
+    constructor(scene) {
+        super(scene);
+        this.initBuffers();
+    }
+
+    initBuffers() {
+        this.vertices = [
+            0, 0, 0,
+            2, 0, 0,
+            0, 2, 0
+        ];
+
+        this.indices = [
+			0, 1, 2,
+		];
+
+        this.primitiveType = this.scene.gl.TRIANGLES;
+
+		this.initGLBuffers();
+    }
+}
